d00ab4782e4e6ac6b4bd0aae3da8879a1bf28cfa
Alexandre Ribeiro <alexandresillva@gmail.com>
2023-08-21 23:03:54 -0300

    feat: forms

--- apps/pages/urls.py
+++ apps/pages/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
+from apps.blog.views import BlogCreateView
 from apps.pages.views import HomePageView, AboutPageView
 
 urlpatterns = [
     path("", HomePageView.as_view(), name='home'),
-    path("about/", AboutPageView.as_view(), name='about')
+    path("about/", AboutPageView.as_view(), name='about'),
+    path("blog/post/new/", BlogCreateView.as_view(), name="post_new"),
 
 ]
